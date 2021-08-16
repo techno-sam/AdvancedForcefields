29e109c1995283b7b8bd836dad78117bfed7a862
techno_sam <linux.techno.sam@gmail.com>
2021-08-16 10:44:30 -0700

    Redo complete logic system, create textures.

--- src/main/java/com/slimeist/aforce/common/blocks/ForceControllerBlock.java
+++ src/main/java/com/slimeist/aforce/common/blocks/ForceControllerBlock.java
@@ -1,15 +1,45 @@
 package com.slimeist.aforce.common.blocks;
 
 import com.slimeist.aforce.common.tiles.ForceControllerTileEntity;
+import com.slimeist.aforce.common.tiles.ForceNetworkTileEntity;
+import com.slimeist.aforce.core.interfaces.IForceNetworkBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.material.PushReaction;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
 
-public class ForceControllerBlock extends Block {
+public class ForceControllerBlock extends HorizontalBlock implements IForceNetworkBlock {
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
     public ForceControllerBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.defaultBlockState()
+                .setValue(POWERED, false)
+                .setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING);
+        builder.add(POWERED);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_) {
+        return this.defaultBlockState().setValue(FACING, p_196258_1_.getHorizontalDirection().getOpposite());
     }
 
     @Override
@@ -20,5 +50,59 @@ public class ForceControllerBlock extends Block {
     @Override
     public TileEntity createTileEntity(BlockState block, IBlockReader world) {
         return new ForceControllerTileEntity();
+    }
+
+    @Override
+    public void onPlace(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        super.onPlace(state, worldIn, pos, oldState, isMoving);
+        this.doUpdate(state, worldIn, pos);
+    }
+
+        @Override
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
+        this.doUpdate(state, worldIn, pos);
+    }
+
+    private void doUpdate(BlockState state, World worldIn, BlockPos pos) {
+        TileEntity myTile = worldIn.getBlockEntity(pos);
+        if (myTile instanceof ForceControllerTileEntity) {
+            boolean powered = worldIn.hasNeighborSignal(pos);
+            if (powered!=state.getValue(POWERED)) {
+                worldIn.setBlock(pos, state.setValue(POWERED, powered), Constants.BlockFlags.BLOCK_UPDATE);
+                ForceControllerTileEntity controllerTile = (ForceControllerTileEntity) myTile;
+                if (powered) {
+                    controllerTile.onPowered();
+                } else {
+                    controllerTile.onDepowered();
+                }
+            }
+        }
+    }
+
+    public PushReaction getPistonPushReaction(BlockState p_149656_1_) {
+        return PushReaction.BLOCK;
+    }
+
+    @Override
+    public int getDistance(World world, BlockPos pos) {
+        TileEntity tile = world.getBlockEntity(pos);
+        if (tile instanceof ForceNetworkTileEntity) {
+            return ((ForceNetworkTileEntity) tile).getDistance();
+        }
+        return -1;
+    }
+
+    @Override
+    public void setDistance(World world, BlockPos pos, int distance) {
+        TileEntity tile = world.getBlockEntity(pos);
+        if (tile instanceof ForceNetworkTileEntity) {
+            ((ForceNetworkTileEntity) tile).setDistance(distance);
+        }
+    }
+
+    @Override
+    public boolean hasCloser(World world, BlockPos pos, BlockPos bannedPos, int distance) {
+        return false;
     }
 }
