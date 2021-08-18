1a2687ce631c86357b13638989e7dcae520bb30a
techno_sam <linux.techno.sam@gmail.com>
2021-08-17 17:34:36 -0700

    Complete force controller model, prepare ForceControllerBlock for containerhood

--- src/main/java/com/slimeist/aforce/common/blocks/ForceControllerBlock.java
+++ src/main/java/com/slimeist/aforce/common/blocks/ForceControllerBlock.java
@@ -5,21 +5,28 @@ import com.slimeist.aforce.common.tiles.ForceNetworkTileEntity;
 import com.slimeist.aforce.core.interfaces.IForceNetworkBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ContainerBlock;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.PushReaction;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.util.Constants;
 
-public class ForceControllerBlock extends HorizontalBlock implements IForceNetworkBlock {
+import javax.annotation.Nullable;
+import java.util.Random;
+
+public class ForceControllerBlock extends Block implements IForceNetworkBlock {
     public static final DirectionProperty FACING = HorizontalBlock.FACING;
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
@@ -49,6 +56,12 @@ public class ForceControllerBlock extends HorizontalBlock implements IForceNetwo
 
     @Override
     public TileEntity createTileEntity(BlockState block, IBlockReader world) {
+        return this.newBlockEntity(world);
+    }
+
+    @Nullable
+    //@Override
+    public TileEntity newBlockEntity(IBlockReader world) {
         return new ForceControllerTileEntity();
     }
 
@@ -104,5 +117,34 @@ public class ForceControllerBlock extends HorizontalBlock implements IForceNetwo
     @Override
     public boolean hasCloser(World world, BlockPos pos, BlockPos bannedPos, int distance) {
         return false;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState state, World world, BlockPos pos, Random rnd) {
+        if (state.getValue(POWERED)) {
+            double d0 = (double)pos.getX() + 0.5D;
+            double d1 = (double)pos.getY();
+            double d2 = (double)pos.getZ() + 0.5D;
+            if (rnd.nextDouble() < 0.1D) {
+                world.playLocalSound(d0, d1, d2, SoundEvents.BLASTFURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+            }
+
+            Direction direction = state.getValue(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+            double d3 = 0.52D;
+            double d4 = rnd.nextDouble() * 0.6D - 0.3D;
+            double d5 = direction$axis == Direction.Axis.X ? (double)direction.getStepX() * 0.52D : d4;
+            double d6 = rnd.nextDouble() * 9.0D / 16.0D;
+            double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getStepZ() * 0.52D : d4;
+            world.addParticle(ParticleTypes.PORTAL, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    public BlockState rotate(BlockState p_185499_1_, Rotation p_185499_2_) {
+        return p_185499_1_.setValue(FACING, p_185499_2_.rotate(p_185499_1_.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState p_185471_1_, Mirror p_185471_2_) {
+        return p_185471_1_.rotate(p_185471_2_.getRotation(p_185471_1_.getValue(FACING)));
     }
 }
