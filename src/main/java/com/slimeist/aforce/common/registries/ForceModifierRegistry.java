13caee030f31820630995a8bc2f3b74ea46ffa4d
techno_sam <linux.techno.sam@gmail.com>
2022-07-15 18:12:04 -0700

    Fix tag system

--- src/main/java/com/slimeist/aforce/common/registries/ForceModifierRegistry.java
+++ src/main/java/com/slimeist/aforce/common/registries/ForceModifierRegistry.java
@@ -7,25 +7,31 @@ import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tags.ITag;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
+import java.util.function.Supplier;
+
 public class ForceModifierRegistry extends ForgeRegistryEntry<ForceModifierRegistry> {
-    protected final Ingredient trigger;
+    protected final Supplier<Ingredient> trigger;
     protected final IForceModifierAction action;
 
     public ForceModifierRegistry(ITag<Item> trigger, IForceModifierAction action) {
-        this(Ingredient.of(trigger), action);
+        this(() -> Ingredient.of(trigger), action);
     }
 
     public ForceModifierRegistry(Item trigger, IForceModifierAction action) {
-        this(Ingredient.of(trigger), action);
+        this(() -> Ingredient.of(trigger), action);
     }
 
     public ForceModifierRegistry(Ingredient trigger, IForceModifierAction action) {
+        this(() -> trigger, action);
+    }
+
+    public ForceModifierRegistry(Supplier<Ingredient> trigger, IForceModifierAction action) {
         this.trigger = trigger;
         this.action = action;
     }
 
     public boolean matches(ItemStack test) {
-        return this.trigger.test(test);
+        return this.trigger.get().test(test);
     }
 
     public IForceModifierAction getAction() {
